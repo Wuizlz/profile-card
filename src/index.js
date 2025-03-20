cfa1b88845a867574d15521838871c37ec363a3f
Daniel <Brisenodan12@gmail.com>
2025-03-20 11:09:54 -0500

    Initial commit - Profile Card Project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="./photocard.jpg" alt="Daniel Briseno" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div className="intro">
+      <h1>Daniel Briseno</h1>
+      <p>
+        Full-stack web developer and soon to be app developer. I would like to
+        then create my own app SchoolEm and change lives. I love to learn, spend time with my gf, eat,
+        enjoy nature, listen to music, and workout.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML + CSS" level="💪" color="red" />
+      <Skill skill="JavaScript" level="💪" color="blue" />
+      <Skill skill="Web Design" level="💻" color="green" />
+      <Skill skill="Git and GitHub" level="👍" color="orange" />
+      <Skill skill="React" level="⚛️" color="lightblue" />
+      <Skill skill="Java" level="🔥" color="orangered" />
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>{level}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
